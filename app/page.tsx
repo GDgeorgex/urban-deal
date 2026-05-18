ca35a135bf0986dd206b45d44c67feac95d9af81
GDgeorgex <george13davy@gmail.com>
2026-05-19 00:31:30 +0200

    Update again.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,17 @@ import {
   AboutSection,
   ContactSection,
 } from "@/components/home-sections"
-import { CmsContent } from "../layout"; // Import the CmsContent type
 
-export default function HomePage({ cmsContent }: { cmsContent: CmsContent }) {
+export default function HomePage() {
   return (
     <>
       <Navbar />
       <main>
-        <Hero cmsContent={cmsContent} />
-        <PreorderStrip cmsContent={cmsContent} />
-        <FeaturedSection cmsContent={cmsContent} />
-        <AboutSection cmsContent={cmsContent} />
-        <ContactSection cmsContent={cmsContent} />
+        <Hero />
+        <PreorderStrip />
+        <FeaturedSection />
+        <AboutSection />
+        <ContactSection />
       </main>
       <Footer />
     </>
